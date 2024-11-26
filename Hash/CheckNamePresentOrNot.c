24105b0c04a8d9b3973eb9d03988386e068300d5
amitkumarauddy <amitkumarauddy@gmail.com>
2024-11-26 17:59:50 +0530

    code

---
+++ Hash/CheckNamePresentOrNot.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+
+char* hashSet[] = {NULL, "Jones", NULL, "Lisa", NULL, "Bob", NULL, "Siri", "Pete", NULL};
+
+int hashFunction(const char* value){
+    int sum = 0;
+    for(int i = 0; i < strlen(value); i++){
+        sum = sum + (int)value[i];
+    }
+    return sum % 10;
+}
+
+
+int contains(char* name){
+    int index = hashFunction(name);
+    if(hashSet[index] != NULL && strcmp(hashSet[index],name) == 0){
+        return 1;
+    }
+    return 0;
+}
+
+int main(){
+    printf("'Amit' is present in the Hash Set : %s", contains("Amit")?"true":"false");
+    return 0;
+}
--- Test/Fibonacci.c
+++ Test/Fibonacci.c
@@ -1,22 +1,18 @@
 #include<stdio.h>
 
-int main(){
-    int n,t1,t2,t3,i;
-    
-    t1 = 0;
-    t2 = 1;
-    t3 = t1 + t2;
+int main() {
+    int numElements, firstTerm = 0, secondTerm = 1, nextTerm, i;
     
     printf("Enter number of elements: ");
-    scanf("%d", &n);
+    scanf("%d", &numElements);
     
-    printf("Fibonacci Series : %d %d ",t1,t2);
+    printf("Fibonacci Series: %d %d ", firstTerm, secondTerm);
     
-    for(i = 3; i<=n; i++){
-        printf("%d ",t3);
-        t1 = t2;
-        t2 = t3;
-        t3 = t1 + t2;
+    for (i = 3; i <= numElements; i++) {
+        nextTerm = firstTerm + secondTerm;
+        printf("%d ", nextTerm);
+        firstTerm = secondTerm;
+        secondTerm = nextTerm;
     }
     return 0;
 }
