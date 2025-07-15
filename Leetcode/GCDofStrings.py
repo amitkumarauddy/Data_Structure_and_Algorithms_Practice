d8eb2f1cbafd31291994c7e51421bd16aee6e8bb
amitkumarauddy <amitkumarauddy@gmail.com>
2025-07-15 13:25:24 +0530

    GCD of Strings

---
+++ Leetcode/GCDofStrings.py
@@ -0,0 +1,10 @@
+def GCDofStrings(str1, str2) ->str:
+    if str1 + str2 != str2 + str1:
+        return "no gcd"
+    if len(str1) == len(str2):
+        return str1
+    if len(str1) > len(str2):
+        return GCDofStrings(str1[len(str2):], str2)
+    return GCDofStrings(str1, str2[len(str1):]) 
+
+print(GCDofStrings("ABABAB","ABAB"))
