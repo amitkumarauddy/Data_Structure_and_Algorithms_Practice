9e54d64df02f6e7c6762b5a578efec352dcacecb
amitkumarauddy <amitkumarauddy@gmail.com>
2025-07-17 19:03:18 +0530

    Max Sub Array

---
+++ Leetcode/MaxSubArray.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def findMaxAverage(nums, k):
+        window_sum = sum(nums[:k])
+        max_sum = window_sum
+
+        for i in range(k, len(nums)):
+            window_sum += nums[i] - nums[i - k]
+            max_sum = max(max_sum, window_sum)
+
+        return max_sum / float(k)
+        
+    
+    print(findMaxAverage([1, 12, -5, -6, 50, 3], 4))
